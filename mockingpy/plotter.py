6a08188bfc82c5bbe74c87e4813d515cbd438bfa
Chia-vie <christine.ackerl@univie.ac.at>
2021-09-13 17:56:40 +0200

    preliminary version of Maps

--- mockingpy/plotter.py
+++ mockingpy/plotter.py
@@ -5,6 +5,7 @@ import glob
 import os
 import pandas as pd
 from matplotlib.colors import LogNorm
+import vaex
 
 class PlotSpec():
     def __init__(self,files):
@@ -57,6 +58,94 @@ class AgeMet():
         ax.set_ylabel('Metallicity')
         plt.colorbar(map, label='Mass')
         plt.show()
+
+class Maps():
+    def __init__(self, file):
+        self.file = file
+        self.path = os.getcwd()
+        self.data = vaex.from_ascii(self.path + '/' + self.file)
+
+    def make(self, valcol, selcol=None, seltype=None, limit=None, exclude=None, limits=[[-200, 200], [-200, 200]], shape=(50, 50)):
+        '''
+        :param valcol: The column name of the values to map
+        :return: None, plots heatmap
+        '''
+        if not seltype == None:
+            if seltype.lower() == 'l':
+                selection = self.data[selcol] > limit
+                title = f'{selcol}>{limit}'
+            elif seltype.lower() == 'u':
+                selection = self.data[selcol] < limit
+                title = f'{selcol}<{limit}'
+            elif seltype.lower() == 'e':
+                selection = self.data[selcol] != exclude
+                title = f'Excluded: {selcol} = {exclude}'
+        else:
+            selection = None
+            title = 'All'
+        self.valcol = valcol
+        self.values= self.data.mean(self.data[self.valcol], binby=[self.data.x, self.data.y], limits=limits, shape=shape, selection=selection)
+        fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+        map =  plt.imshow(self.values.T, origin='lower', extent=[-200,200,-200,200])
+        ax.set_title(title)
+        ax.set_xlabel('x [kpc]')
+        ax.set_ylabel('y [kpc]')
+        plt.colorbar(map, label=self.valcol)
+        plt.show()
+
+    def satellites(self, name):
+        self.name = name
+        # Group by nsat, raname nsat column, join grouped dataset and old one
+        groupeddata = self.data.groupby(by='nsat',
+                        agg={'lboltot': vaex.agg.sum('lbol')})
+        groupeddata.rename('nsat','groupednsat')
+        joined=self.data.join(groupeddata, left_on='nsat', right_on='groupednsat')
+
+        # remove renamed nsat column, is not needed anymore.
+        joined.drop('groupednsat')
+
+        # calculate the weights
+        joined['lumweight'] = joined['lbol']/joined['lboltot']
+
+        # calculate the radii and weighted radii
+        joined['radius'] = np.sqrt(joined['x']**2+joined['y']**2+joined['z']**2)
+        joined['weightrad'] = joined['lumweight']*joined['radius']
+
+        #extract the data we want
+        result=joined.groupby(by='nsat', agg={'mean_weightrad': vaex.agg.sum('weightrad'),
+                             'mean_rad': vaex.agg.mean('radius'),
+                             'tac': vaex.agg.mean('tac'),
+                             'mtot': vaex.agg.mean('mtot')})
+        tac = result.evaluate("tac")
+        meanweightrad = result.evaluate("mean_weightrad")
+        #meanrad = result.evaluate('mean_rad')
+        mtot = result.evaluate('mtot')
+
+        #plot weighted radii against tac and mtot
+        fig = plt.figure(figsize = [18,7])
+        fig.suptitle(name, size = 20)
+
+        ax1 = fig.add_subplot(121)
+        ax1.set_xlabel('$r_{mean}$ [kpc]', size = 20)
+        ax1.set_ylabel('$t_{acc}$ [Gyr ago]', size = 20)
+        ax1.set_ylim(0,12.8)
+        ax1.set_xlim(0,600)
+
+        ax2 = fig.add_subplot(122)
+        ax2.set_xlabel('$r_{mean}$ [kpc]', size = 20)
+        ax2.set_ylabel('$log(m_{tot})$ [$M_{sun}$]', size = 20)
+        ax2.set_yscale('log')
+        ax2.set_xlim(0,600)
+        ax2.set_ylim(10**8,10**11)
+
+        a = ax1.scatter(meanweightrad, tac, c=np.log10(mtot), s=30, vmin=8, vmax=11, cmap='inferno')
+        b = ax2.scatter(meanweightrad, mtot, c=tac, s=30, vmin=0, vmax=12.8,cmap='inferno')
+        fig.colorbar(a, ax=ax1)
+        fig.colorbar(b, ax=ax2).set_label(label='$t_{acc}$ [Gyr ago]',size=20)
+        plt.subplots_adjust(wspace=0.1)
+        plt.show()
+
+
 
 
 
